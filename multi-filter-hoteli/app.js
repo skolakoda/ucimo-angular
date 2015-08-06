cc1474e745f66d4f12633fc81c12761684010b4b
Damjan Pavlica <mudroljub@gmail.com>
2015-08-06 19:09:05 +0200

    hotel filter

--- multi-filter-hoteli/app.js
+++ multi-filter-hoteli/app.js
@@ -1,49 +1,57 @@
+// racuna i rec u naslovu
+
 angular.module("App", [])
     .controller('HoteliKontrol', HoteliKontrol);
 
 
 function HoteliKontrol($scope) {
     $scope.kriteriji = {
-        vrsta1: 'skup',
-        vrsta2: ''
+		//skupoca: "skup",
+		slobodan: true
     };
 
     $scope.hotels = [
         {
-            ime: 'the taj hotel',
+            ime: 'skup hotel',
             ocena: 5,
-            vrsta: 'skup',
-            cena: 5675
+            vrsta: 'jeftin',
+            cena: 5675,
+			slobodan: true
         },
         {
             ime: 'vivanta Palace',
             ocena: 5,
             vrsta: 'skup',
-            cena: 8670
+            cena: 8670,
+			slobodan: true
         },
         {
             ime: 'aviary',
             ocena: 4,
             vrsta: 'jeftin',
-            cena: 3000
+            cena: 3000,
+			slobodan: true
         },
         {
             ime: 'dummy',
             ocena: 4,
             vrsta: 'dummy',
-            cena: 33333100
+            cena: 33333100,
+			slobodan: true
         },
         {
             ime: 'good guest',
             ocena: 3,
             vrsta: 'jeftin',
-            cena: 3500
+            cena: 3500,
+			slobodan: false
         },
         {
             ime: 'the ramada',
             ocena: 3,
             vrsta: 'skup',
-            cena: 7500
+            cena: 7500,
+			slobodan: false
         }
     ];
 }   // HoteliKontrol
