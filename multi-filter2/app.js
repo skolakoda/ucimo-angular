688dd0e7c46eed5aea62df46cddf3b5feab05c8b
mudroljub <mudroljub@gmail.com>
2015-08-06 14:05:36 +0200

    filteri sredjivanje

---
+++ multi-filter2/app.js
@@ -0,0 +1,46 @@
+angular.module("App", [])
+    .controller('HoteliKontrol', HoteliKontrol);
+
+
+function HoteliKontrol($scope) {
+    $scope.search=[];
+
+    $scope.hotels = [
+        {
+            ime: 'the taj hotel',
+            ocena: 5,
+            vrsta: 'skup',
+            cena: 5675
+        },
+        {
+            ime: 'vivanta Palace',
+            ocena: 5,
+            vrsta: 'skup',
+            cena: 8670
+        },
+        {
+            ime: 'aviary',
+            ocena: 4,
+            vrsta: 'jeftin',
+            cena: 3000
+        },
+        {
+            ime: 'dummy',
+            ocena: 4,
+            vrsta: 'dummy',
+            cena: 33333100
+        },
+        {
+            ime: 'good guest',
+            ocena: 3,
+            vrsta: 'jeftin',
+            cena: 3500
+        },
+        {
+            ime: 'the ramada',
+            ocena: 3,
+            vrsta: 'skup',
+            cena: 7500
+        }
+    ];
+}   // HoteliKontrol
