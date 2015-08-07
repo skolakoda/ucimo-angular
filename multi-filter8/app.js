a0d7c740e5d862e43864d17a6fe35b366fc8a2e5
mudroljub <mudroljub@gmail.com>
2015-08-07 10:10:00 +0200

    pomalo

--- multi-filter8/app.js
+++ multi-filter8/app.js
@@ -1,14 +1,19 @@
-function ShoppingCartCtrl($scope)  {
-    
-        $scope.items = [
-			{Name: "Soap", Price: "25", Quantity: "10"},
-			{Name: "Shaving cream", Price: "50", Quantity: "15"},
-			{Name: "Shampoo", Price: "100", Quantity: "5"}
-		];
-			
-		$scope.mySortFunction = function(item) {
-			if(isNaN(item[$scope.sortExpression]))
-				return item[$scope.sortExpression];
-			return parseInt(item[$scope.sortExpression]);
-		}
+angular.module('radnja', [])
+	.controller('ProizvodiKontrol', ProizvodiKontrol)
+
+
+function ProizvodiKontrol()  {
+	var proizvodi = this;
+
+	proizvodi.spisak = [
+		{Ime: "Sapun", Cena: "25", Kolicina: "10"},
+		{Ime: "Krema", Cena: "50", Kolicina: "15"},
+		{Ime: "Shampon", Cena: "100", Kolicina: "5"}
+	];
+
+	proizvodi.sortiraj = function(item) {
+		if(isNaN(item[proizvodi.kriterijSortiranja]))
+			return item[proizvodi.kriterijSortiranja];
+		return parseInt(item[proizvodi.kriterijSortiranja]);
+	}
 }
