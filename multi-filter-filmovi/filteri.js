dc3b18795aa68f631e48bf86868106c73212b156
mudroljub <mudroljub@gmail.com>
2015-08-07 11:22:14 +0200

    m

--- multi-filter-filmovi/filteri.js
+++ multi-filter-filmovi/filteri.js
@@ -1,21 +1,24 @@
+// uporedjuje nazive atributa u izabraniZanrovi sa strunama zanrova filma
+
 angular
     .module('multiFilter')
-    .filter('proberiPoZanru', proberiPoZanru);
+    .filter('zanrFilter', zanrFilter);
 
 
-function proberiPoZanru() {
+function zanrFilter() {
     return function(sviFilmovi, izabraniZanrovi) {
         var podaci = {
             izabraniZanrovi: izabraniZanrovi,
             odabraniFilmovi: []
         };
-        angular.forEach(sviFilmovi, proveriZanr, podaci);   // podaci su this
+        angular.forEach(sviFilmovi, proveriZanr, podaci);   // podaci su unutra this
         return podaci.odabraniFilmovi;
     };
-}   // proberiPoZanru
+}   // zanrFilter
 
 
 function proveriZanr(film) {
+    // ako je zanr ovog filma u izabranim zanrovima
     if (this.izabraniZanrovi[film.zanr] === true) {
         this.odabraniFilmovi.push(film);
     }
