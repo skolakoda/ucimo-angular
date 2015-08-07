c761479c353f9371c446c377a9c9f9aa79639674
mudroljub <mudroljub@gmail.com>
2015-08-07 11:55:36 +0200

    pomalo

--- multi-filter-igraci/app.js
+++ multi-filter-igraci/app.js
@@ -1,1 +1,30 @@
+// problem, kada pritisnem i pustim istaknuto, prikazuje samo false (u pocetku prikazuje sve)
+// resenje koristiti ng-false-value
+// checkbox moze da prikazuje iskljucivo true/false ili ukljucivo true/sve
+
+
 angular.module('myApp', [])
+    .controller('IgraciKontrol', IgraciKontrol)
+
+
+function IgraciKontrol() {
+    var igraci = this;
+    igraci.trazeno = '';
+    igraci.kriteriji = {};
+    igraci.kriteriji.istaknut = true;
+
+    // u servis!
+    igraci.spisak = [
+        {id: 1, ime: 'Dallas Mavericks', sport: 'Basketball', grad: 'Dallas', istaknut: true},
+        {id: 2, ime: 'Dallas Cowboys', sport: 'Football', grad: 'Dallas', istaknut: false},
+        {id: 3, ime: 'New York Knicks', sport: 'Basketball', grad: 'New York', istaknut: false},
+        {id: 4, ime: 'Brooklyn Nets', sport: 'Basketball', grad: 'New York', istaknut: false},
+        {id: 5, ime: 'New York Jets', sport: 'Football', grad: 'New York', istaknut: false},
+        {id: 6, ime: 'New York Giants', sport: 'Football', grad: 'New York', istaknut: true},
+        {id: 7, ime: 'Los Angeles Lakers', sport: 'Basketball', grad: 'Los Angeles', istaknut: true},
+        {id: 8, ime: 'Los Angeles Clippers', sport: 'Basketball', grad: 'Los Angeles', istaknut: false},
+        {id: 9, ime: 'Dallas Stars', sport: 'Hockey', grad: 'Dallas', istaknut: false},
+        {id: 10, ime: 'Boston Bruins', sport: 'Hockey', grad: 'Boston', istaknut: true}
+    ];
+    igraci.gradovi = ['Dallas', 'Los Angeles', 'Boston', 'New York'];
+}   // IgraciKontrol
