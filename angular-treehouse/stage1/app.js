9ab4c587c6dca482b1c1b9cdef0112e404ae369d
mudroljub <mudroljub@gmail.com>
2015-08-15 13:04:16 +0200

    cistka

--- angular-treehouse/stage1/app.js
+++
@@ -1,6 +0,0 @@
-angular.module('treehouseCourse', [])
-  .controller('stageOneCtrl', function ($scope, $http) {
-    $http.get('people.json').success(function(people) {
-      $scope.people = people;
-    });
-  });
