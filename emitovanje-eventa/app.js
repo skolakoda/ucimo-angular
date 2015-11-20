3ca8ac891d1c293468bd7ed93b8d07c1037a9148
mudroljub <mudroljub@gmail.com>
2015-11-20 17:33:36 +0100

    remove unused

--- emitovanje-eventa/app.js
+++ emitovanje-eventa/app.js
@@ -1,10 +1,10 @@
 angular.module('MyApp', [])
-    .controller('ListCtrl', ['$scope', '$rootScope', '$http', 'usersService', ListCtrl])
-	.controller('DetailCtrl', ['$scope', 'usersService', DetailCtrl])
-    .factory('usersService', usersService);
+    .controller('ListCtrl', ['$scope', '$rootScope', '$http', ListCtrl])
+	.controller('DetailCtrl', ['$scope', DetailCtrl]);
 
 
 function ListCtrl($scope, $rootScope, $http, usersService) {
+
 	$http.get('./users.json').success(function (users) {
 		$scope.users = users;
 	});
@@ -12,25 +12,14 @@ function ListCtrl($scope, $rootScope, $http, usersService) {
 	$scope.selectUser = function (user) {
 		$rootScope.$broadcast('userChanged', user);
 	};
+
 }   // ListCtrl
 
 
 function DetailCtrl($scope, usersService) {
+
 	$scope.$on('userChanged', function (event, user) {
 		$scope.currentUser = user;
 	});
+
 }   // DetailCtrl
-
-
-// izgleda da nicemu ne sluzi
-function usersService() {
-	// var user;
-	// return {
-	// 	setCurrentUser: function (current) {
-	// 		user = current;
-	// 	},
-	// 	getCurrentUser: function () {
-	// 		return user;
-	// 	}
-	// };
-}   // usersService
