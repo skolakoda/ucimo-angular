cb3d08cb047eb9892a62bc3531b7120eac669a03
mudroljub <mudroljub@gmail.com>
2015-11-20 21:55:36 +0100

    obrisan visak

--- emitovanje-eventa/app.js
+++ emitovanje-eventa/app.js
@@ -1,25 +1,27 @@
 angular.module('MyApp', [])
     .controller('ListCtrl', ['$scope', '$rootScope', '$http', ListCtrl])
-	.controller('DetailCtrl', ['$scope', DetailCtrl]);
+    .controller('DetailCtrl', ['$scope', DetailCtrl]);
 
 
-function ListCtrl($scope, $rootScope, $http, usersService) {
+function ListCtrl($scope, $rootScope, $http) {
 
-	$http.get('./users.json').success(function (users) {
-		$scope.users = users;
-	});
+    $http
+        .get('./users.json')
+        .success(function(users) {
+            $scope.users = users;
+        });
 
-	$scope.selectUser = function (user) {
-		$rootScope.$broadcast('userChanged', user);
-	};
+    $scope.selectUser = function(user) {
+        $rootScope.$broadcast('userChanged', user);
+    };
 
-}   // ListCtrl
+} // ListCtrl
 
 
-function DetailCtrl($scope, usersService) {
+function DetailCtrl($scope) {
 
-	$scope.$on('userChanged', function (event, user) {
-		$scope.currentUser = user;
-	});
+    $scope.$on('userChanged', function(event, user) {
+        $scope.currentUser = user;
+    });
 
-}   // DetailCtrl
+} // DetailCtrl
