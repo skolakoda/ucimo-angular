a54853a8f42a7f670b6ebe4ee44e77bf6ae3e05e
mudroljub <mudroljub@gmail.com>
2015-11-21 22:54:17 +0100

    cistka

--- routing-example/app/beans/render-context.js
+++ routing-example/app/beans/render-context.js
@@ -1,82 +1,50 @@
-(function( ng, app ) {
-	
-	"use strict";
+(function(ng, app) {
 
-	// I provide information about the current route request, local to the given render path. 
-	app.value(
-		"RenderContext",
-		function( requestContext, actionPrefix, paramNames ) {
+    "use strict";
 
+    // provide information about the current route request, local to the given render path.
+    app.value(
+        "RenderContext",
+        function(requestContext, actionPrefix, paramNames) {
 
-			// I return the next section after the location being watched.
-			function getNextSection() {
+            // return the next section after the location being watched.
+            function getNextSection() {
+                return (
+                    requestContext.getNextSection(actionPrefix)
+                );
+            }
 
-				return(
-					requestContext.getNextSection( actionPrefix )
-				);
-
-			}
-
-
-			// I check to see if the action has changed (and is local to the current location).
-			function isChangeLocal() {
-
-				return(
-					requestContext.startsWith( actionPrefix )
-				);
-
-			}
+            // check to see if the action has changed (and is local to the current location).
+            function isChangeLocal() {
+                return (
+                    requestContext.startsWith(actionPrefix)
+                );
+            }
 
 
-			// I determine if the last change in the request context is relevant to
-			// the action and route params being observed in this render context.
-			function isChangeRelevant() {
-
-				// If the action is not local to the action prefix, then we don't even 
-				// want to bother checking the params.
-				if ( ! requestContext.startsWith( actionPrefix ) ) {
-
-					return( false );
-
-				}
-
-				// If the action has changed, we don't need to bother checking the params.
-				if ( requestContext.hasActionChanged() ) {
-
-					return( true ) ;
-
-				}
-
-				// If we made it this far, we know that the action has not changed. As such, we''ll 
-				// have to make the change determination based on the observed parameters.
-				return(
-					paramNames.length && 
-					requestContext.haveParamsChanged( paramNames )
-				);
-
-			}
+            // determine if the last change in the request context is relevant
+            function isChangeRelevant() {
+                if (!requestContext.startsWith(actionPrefix)) {
+                    return (false);
+                }
+                if (requestContext.hasActionChanged()) {
+                    return (true);
+                }
+                return (
+                    paramNames.length &&
+                    requestContext.haveParamsChanged(paramNames)
+                );
+            }	// isChangeRelevant
 
 
-			// ---------------------------------------------- //
-			// ---------------------------------------------- //
+            // Return the public API.
+            return ({
+                getNextSection: getNextSection,
+                isChangeLocal: isChangeLocal,
+                isChangeRelevant: isChangeRelevant
+            });
 
-			
-			// Private variables...
+        }
+    );
 
-
-			// ---------------------------------------------- //
-			// ---------------------------------------------- //
-
-
-			// Return the public API.
-			return({
-				getNextSection: getNextSection,
-				isChangeLocal: isChangeLocal,
-				isChangeRelevant: isChangeRelevant
-			});
-
-
-		}
-	);
-
-})( angular, Demo );
+})(angular, Demo);
