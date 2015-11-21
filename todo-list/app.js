129d8ac518c897ac040163a0739efbc306b7d94d
mudroljub <mudroljub@gmail.com>
2015-11-21 23:26:46 +0100

    cistka

---
+++ todo-list/app.js
@@ -0,0 +1,50 @@
+var model = {
+    user: "Adam"
+};
+
+angular.module("todoApp", [])
+
+.run(function($http) {
+    $http.get("todo.json").success(function(data) {
+        model.items = data;
+    });
+})
+
+.filter("checkedItems", function() {
+    return function(items, showComplete) {
+        var resultArr = [];
+        angular.forEach(items, function(item) {
+
+            if (item.done == false || showComplete == true) {
+                resultArr.push(item);
+            }
+        });
+        return resultArr;
+    }
+})
+
+.controller("ToDoCtrl", function($scope) {
+    $scope.todo = model;
+
+    $scope.incompleteCount = function() {
+        var count = 0;
+        angular.forEach($scope.todo.items, function(item) {
+            if (!item.done) {
+                count++
+            }
+        });
+        return count;
+    }
+
+    $scope.warningLevel = function() {
+        return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
+    }
+
+    $scope.addNewItem = function(actionText) {
+        $scope.todo.items.push({
+            action: actionText,
+            done: false
+        });
+    }
+
+});
