33c507da972304e773ef92c491ea37ecdbb81551
mudroljub <mudroljub@gmail.com>
2015-11-22 20:57:09 +0100

    obrisan visak

--- wikipedia-search/app.js
+++ wikipedia-search/app.js
@@ -2,24 +2,22 @@
 angular.module('WikiApp', [])
     .controller('MainCtrl', MainCtrl);
 
-function MainCtrl($scope, $http, $timeout) {
-    $scope.results = [];
+function MainCtrl($scope, $http) {
 
     $scope.search = function() {
+        if(!$scope.searchTxt) return;
         $scope.results = [];
         var apiUrl = 'http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&callback=JSON_CALLBACK&gsrsearch=' + $scope.searchTxt;
-        var page = 'http://en.wikipedia.org/?curid=';
 
         $http.jsonp(apiUrl)
             .success(function(data) {
-                var results = data.query.pages;
-                angular.forEach(results, function(v, k) {
+                angular.forEach(data.query.pages, function(page, pageId) {
                     $scope.results.push({
-                        title: v.title,
-                        body: v.extract,
-                        page: page + v.pageid
+                        title: page.title,
+                        extract: page.extract,
+                        pageUrl: 'http://en.wikipedia.org/?curid=' + pageId
                     });
                 });
-            });
+            }); // end success
     };
-}
+}   // MainCtrl
