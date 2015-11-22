1a5f754f427aee16e13f7c1cd3c59a31ee3020df
mudroljub <mudroljub@gmail.com>
2015-11-22 22:38:18 +0100

    beautify

--- online-radnja/directives.js
+++ online-radnja/directives.js
@@ -1,42 +1,42 @@
 var app = angular.module('store-directives', []);
 
 app.directive("descriptions", function() {
-  return {
-    restrict: "E",
-    templateUrl: "partials/descriptions.html"
-  }
+    return {
+        restrict: "E",
+        templateUrl: "partials/descriptions.html"
+    }
 });
 
 app.directive("reviews", function() {
-  return {
-    restrict: "E",
-    templateUrl: "partials/reviews.html"
-  }
+    return {
+        restrict: "E",
+        templateUrl: "partials/reviews.html"
+    }
 });
 
 app.directive("specs", function() {
-  return {
-    restrict: "E",
-    templateUrl: "partials/specs.html"
-  }
+    return {
+        restrict: "E",
+        templateUrl: "partials/specs.html"
+    }
 });
 
 app.directive("productTabs", function() {
-  return {
-    restrict: "E",
+    return {
+        restrict: "E",
 
-    templateUrl: "partials/product-tabs.html",
-    controller: function() {
-      this.tab = 1;
+        templateUrl: "partials/product-tabs.html",
+        controller: function() {
+            this.tab = 1;
 
-      this.isSet = function(checkTab) {
-        return this.tab === checkTab;
-      };
+            this.isSet = function(checkTab) {
+                return this.tab === checkTab;
+            };
 
-      this.setTab = function(activeTab) {
-        this.tab = activeTab;
-      };
-    },
-    controllerAs: "tab"
-  };
+            this.setTab = function(activeTab) {
+                this.tab = activeTab;
+            };
+        },
+        controllerAs: "tab"
+    };
 });
